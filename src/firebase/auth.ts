560501b4f75073fe1559795bd63ae0d1e1fa5ea2
mironal <miro.gunp@gmail.com>
2018-09-23 08:46:28 +0900

    fix import

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,4 +1,5 @@
-import firebase from "firebase"
+import firebase from "firebase/app"
+import "firebase/auth"
 import { firestoreCollectionReference } from "./firestore"
 
 export const signIn = async () => {
