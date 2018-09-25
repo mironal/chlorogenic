5811a7b091573025c013e8d9ed9fe8e3fb68ad7b
mironal <miro.gunp@gmail.com>
2018-09-25 20:43:35 +0900

    remove unused code

--- src/misc/prelude.ts
+++ src/misc/prelude.ts
@@ -51,22 +51,6 @@ export const addItemAtIndexToArray = <T>(
   return a
 }
 
-/**
- * No side effects.
- */
-export const removeItemAtIndexInArray = <T>(array: T[], index: number) => {
-  // https://redux.js.org/recipes/structuringreducers/immutableupdatepatterns
-  const a = array.slice()
-  a.splice(index, 1)
-  return a
-}
-
-export const removeItemInArray = <T>(array: T[], item: T) =>
-  array.filter(i => i !== item)
-
-export const replaceItemInArray = <T>(array: T[], from: T, to: T) =>
-  array.map(i => (i === from ? to : i))
-
 export type FunctionPropertyNames<T> = {
   [K in keyof T]: T[K] extends Function ? K : never
 }[keyof T]
